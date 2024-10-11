915062eae6c7b0aec193bf5579c98173a7bb5613
krissceron <krissceronb03@gmail.com>
2024-10-10 19:33:24 -0500

    Funciona login y protección de rutas

--- WebAPICrud/obj/Debug/net8.0/WebAPICrud.AssemblyInfo.cs
+++ WebAPICrud/obj/Debug/net8.0/WebAPICrud.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPICrud")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23900cee49ba287a15006e01ed49dbf91ff15cd6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59f53e78fbc223fba1c5a584986dad33674ada6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPICrud")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPICrud")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
